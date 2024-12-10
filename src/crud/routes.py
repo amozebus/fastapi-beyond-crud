cb333086b3d677077037a3f6492331296cdcd40c
amozebus <amozebus@gmail.com>
2024-12-10 17:10:17 +0600

    added simple rate limitation

--- src/crud/routes.py
+++ src/crud/routes.py
@@ -1,23 +1,27 @@
 """CRUD template"""
 from typing import Any, Annotated
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, Request
 
 from db.models import User
 
 from auth.dependencies import get_current_user
 
+from rate_limit import rate_limit
+
 r = APIRouter(prefix="/crud")
 
 
 @r.get("/unprotected", response_model=dict[str, str])
-async def hello_world() -> dict[str, str]:
+@rate_limit(max_calls=5, period=60)
+async def hello_world(request: Request) -> dict[str, str]:
     """Unprotected handler template"""
     return {"message": "Hello, world!"}
 
-
 @r.get("/protected", response_model=dict[str, Any])
+@rate_limit(max_calls=5, period=60)
 async def hello_world_protected(
+    request: Request,
     user: Annotated[User, Depends(get_current_user)]
 ) -> dict[str, Any]:
     """Protected handler template"""
---
+++ src/rate_limit.py
@@ -0,0 +1,39 @@
+import time
+
+from functools import wraps
+
+from typing import Callable, Any
+
+from fastapi import Request, Response, HTTPException
+
+def rate_limit(max_calls: int, period: int):
+    """Limiting requests rate by max_calls/period (in seconds)"""
+    def decorator(func: Callable[[Request], Any]) -> Callable[[Request], Any]:
+        usage: dict[str, list[float]] = {}
+
+        @wraps(func)
+        async def wrapper(request: Request, *args, **kwargs) -> Response:
+            if not request.client:
+                raise ValueError("Request has no client information")
+            ip_address: str = request.client.host
+
+            # update the timestamps
+            now = time.time()
+            if ip_address not in usage:
+                usage[ip_address] = []
+            timestamps = usage[ip_address]
+            timestamps[:] = [t for t in timestamps if now - t < period]
+
+            if len(timestamps) < max_calls:
+                timestamps.append(now)
+                return await func(request, *args, **kwargs)
+
+            wait = period - (now - timestamps[0])
+            raise HTTPException(
+                status_code=429,
+                detail=f"Rate limit exceeded. Retry after {wait:.2f} seconds",
+            )
+
+        return wrapper
+
+    return decorator
