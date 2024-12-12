7ba3f8dc1e540397e2114be049e110f8d88496de
amozebus <amozebus@gmail.com>
2024-12-13 04:45:08 +0600

    added expires_in field to access token response model

--- src/auth/models.py
+++ src/auth/models.py
@@ -1,7 +1,10 @@
 from pydantic import BaseModel
 
+from config import settings
+
 class Token(BaseModel):
     """Token response model"""
 
     access_token: str
+    expires_in: int = settings.ACCESS_TOKEN_EXPIRE * 60
     token_type: str
