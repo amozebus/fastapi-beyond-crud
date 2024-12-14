c83ec0f7b71cd5727e32f4bd9e5825bdc3dc1823
amozebus <amozebus@gmail.com>
2024-12-14 18:01:44 +0600

    removed REDIS_PASSWORD

--- src/db/jti_blocklist.py
+++ src/db/jti_blocklist.py
@@ -5,7 +5,7 @@ import redis.asyncio as aioredis
 from config import settings
 
 REDIS_URL = (
-    f"redis://:{settings.REDIS_PASSWORD}@{settings.REDIS_HOST}:{settings.REDIS_PORT}/0"
+    f"redis://{settings.REDIS_HOST}:{settings.REDIS_PORT}/0"
 )
 
 jti_blocklist = aioredis.from_url(REDIS_URL)
